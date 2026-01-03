6e199cf8357039112960f6916f805d30dc4765a2
David Ayobami <davibams@gmail.com>
2026-01-03 15:24:50 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,15 @@
+const user = JSON.parse(localStorage.getItem("loggedInUser"));
 
+if (!user) {
+    window.location.href = "index.html";
+}
+
+const welcome = document.getElementById("welcome");
+const logoutBtn = document.getElementById("logout-btn");
+
+welcome.textContent = `Welcome, ${user.username}!`;
+
+logoutBtn.addEventListener("click", () => {
+    localStorage.removeItem("loggedInUser");
+    window.location.href = "index.html";
+});
